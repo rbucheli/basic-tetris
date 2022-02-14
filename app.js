0644e1d6cd700dd671030d9e5432352453fb3f42
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-02-14 15:54:00 -0500

    Put all div squares into one array

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMConentLoaded', () => {
-  
-})
+document.addEventListener("DOMConentLoaded", () => {
+  const grid = document.querySelector(".grid")
+  let squares = Array.from(document.querySelectorAll(".grid div"))
+  const ScoreDisplay = document.querySelector("#score")
+  const StartBtn = document.querySelector("#start-but")
+  const width = 10;
+
+  console.log(squares);
+});
+
+
+// console.log(squares) did not work for me. Why?
+
+// let names = ['Rodrigo', 'Sweeney', 'Sookie']
+
+// names.forEach(name => {
+//   console.log(name + ' is the best!')
+// })
