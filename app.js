e1f1fc98fa00e9f19508ceeb691c44e36b8e91af
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-02-23 23:50:49 -0500

    Check arrays with console.log

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMConentLoaded", () => {
   const StartBtn = document.querySelector("#start-but")
   const width = 10;
 
-  // console.log(squares);
   
   // The Tetrominoes
   const lTetromino = [
@@ -15,8 +14,37 @@ document.addEventListener("DOMConentLoaded", () => {
     [width, width*2, width*2+1, width*2+2]
   ]
 
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+  
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0][0]
+  console.log(theTetrominoes[0][0])
 });
-
-
-// console.log(squares) did not work for me. Why?
-
