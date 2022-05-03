0630fbb12a0f795e4765b51f9b954ed8b8d2d53e
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-03 00:06:14 -0400

    Fix l tetronimo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // The Tetrominoes
   const ltetronimo = [
-    [1, width + 1, width * 2 + 1], 
+    [1, width + 1, width * 2 + 1, 2], 
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width*2, width*2+1, width*2+2]
