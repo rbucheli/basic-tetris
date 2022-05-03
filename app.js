c0386c4d7b9efd4fedeb09b957cfa0c81e2aba05
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-03 00:12:31 -0400

    Draw tTetronimo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
-  const width = 10
+  const width = 10 
 
 
   // The Tetrominoes
@@ -14,5 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width*2, width*2+1, width*2+2]
   ]
   
+  const tTetronimo = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
 
 });
