f26ae7c0d07a204225cbf6147deeb7f196343851
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-02 23:55:59 -0400

    Draw l tetronimo

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button')
   const width = 10
 
+
+  // The Tetrominoes
+  const ltetronimo = [
+    [1, width + 1, width * 2 + 1], 
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
   
 
 });
