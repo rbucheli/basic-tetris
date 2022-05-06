db86bad42c52c0f7eec1b6fc3c1cbaae893d3e2c
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-05 23:04:13 -0400

    Add ten more divs to stop tetromino from going past our grid

--- app.js
+++ app.js
@@ -67,4 +67,14 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000)
+
+  // move down function 
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+  }
+
 });
