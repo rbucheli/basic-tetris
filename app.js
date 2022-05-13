afce1e90bf1a4569b97a14a9a5f9b0b353032957
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-12 23:36:14 -0400

    Write function to move tetromino to the right

--- app.js
+++ app.js
@@ -74,6 +74,12 @@ document.addEventListener('DOMContentLoaded', () => {
   function control(e) {
     if (e.keyCode === 37) {
       moveLeft()
+    } else if (e.keyCode === 38) {
+      // rotate
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
     }
   }
   document.addEventListener('keyup', control)
@@ -112,4 +118,19 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  // move the tetromino right, unless it is at the edge or there is a blockage
+  function moveRight() {
+    undraw()
+    const atRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+    
+    if (!atRightEdge) currentPosition += 1
+    
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+
+    draw()
+  }
+
+  
 });
