378c5639e9a5a171a936e35801267f8cc73bca2e
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-16 23:41:46 -0400

    Draw tetrominoes without rotation for display grid and add display shape function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10 
+  let nextRandom = 0
+
 
 
   // The Tetrominoes
@@ -97,10 +99,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       // start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -144,12 +148,26 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // show up-next tetromino in mini-grid display
-
   const displaySquares = document.querySelectorAll('.mini-grid div')
   const displayWidth = 4
   let displayIndex = 0
 
-  // the Tetrominos
+  // the Tetrominos w/o rotations
+  const nextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1],
+    [1, displayWidth, displayWidth + 1, displayWidth + 2],
+    [0, 1, displayWidth, displayWidth + 1],
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]
+  ]
+
+  // display the shape in the mini-grid display
+  function displayShape() {
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    nextTetrominoes[nextRandom].forEach
+  }
 
 
   
