f1a1e591783f0c9cf146a5bd5c430b6d1e7202e4
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-18 22:46:34 -0400

    Display bottom row composition

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10 
   let nextRandom = 0
   let timerId
-
+  let score = 0
 
 
   // The Tetrominoes
@@ -106,6 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4
       draw()
       displayShape()
+      addScore()
     }
   }
 
@@ -185,5 +186,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  
+  // add score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+      
+      if (row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10
+        scoreDisplay.innerHTML = score
+        row.forEach(index => {
+          squares[index].classList.remove('taken')
+        })
+        const squaresRemoved = squares.splice(i, width)
+        console.log(squaresRemoved)
+      }
+    }
+  }
 });
