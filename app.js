672942f91c1fadc1fcab8ff78f3d5f97e2f1a7b7
hum-motor-whir <rodrigo.bucheli@gmail.com>
2022-05-19 23:13:57 -0400

    Add gameOver function

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
       draw()
       displayShape()
       addScore()
+      gameOver()
     }
   }
 
@@ -196,10 +197,21 @@ document.addEventListener('DOMContentLoaded', () => {
         scoreDisplay.innerHTML = score
         row.forEach(index => {
           squares[index].classList.remove('taken')
+          squares[index].classList.remove('tetromino')
         })
         const squaresRemoved = squares.splice(i, width)
-        console.log(squaresRemoved)
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
       }
     }
   }
+
+  //  game over function
+  function gameOver() {
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      scoreDisplay.innerHTML = 'end'
+      clearInterval(timerId)
+    }
+  }
+
 });
